ba4e6a71adc59ba7c0f785471814af3ca61daa45
ZeusOnline1998 <zeusonline1998@gmail.com>
2022-12-30 13:41:21 +0530

    Program for retrieving the data of the given movie url through commandline

---
+++ onemovie.py
@@ -0,0 +1,16 @@
+from page import *
+from scraper import *
+from sys import argv
+
+
+res = get_url_data(argv[1])
+
+soup = get_soup(res)
+
+print(movie_name(soup))
+print(movie_director(soup))
+print(movie_genres(soup))
+print(movie_ratings(soup))
+print(movie_runtime(soup))
+
+
