f9e70fed642b238875e02835766df56543921804
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-01-02 21:32:01 +0530

    Optimised it

--- onemovie.py
+++ onemovie.py
@@ -4,13 +4,9 @@ from sys import argv
 
 
 res = get_url_data(argv[1])
-
 soup = get_soup(res)
-
 print(movie_name(soup))
 print(movie_director(soup))
 print(movie_genres(soup))
 print(movie_ratings(soup))
 print(movie_runtime(soup))
-
-
